4992b7ca011b0f9f1172150721da368fe98a8214
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-12-18 19:30:33 +0700

    add view model untuk  halaman tambah, yang menjadi penghubung antara uitambah dengan backendnya melalui repositoy

---
+++ app/src/main/java/com/example/apisiswa/viewmodel/EntryViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.apisiswa.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.apisiswa.modeldata.DetailSiswa
+import com.example.apisiswa.modeldata.UIStateSiswa
+import com.example.apisiswa.modeldata.toDataSiswa
+import com.example.apisiswa.repositori.RepositoryDataSiswa
+import retrofit2.Response
+
+class EntryViewModel (private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa=uiStateSiswa.detailSiswa):
+        Boolean{
+        return with(uiState){
+            nama.isNotBlank()&&alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa=
+            UIStateSiswa(
+                detailSiswa=detailSiswa,
+                isEntryValid = validasiInput(detailSiswa)
+            )
+    }
+
+    suspend fun addSiswa(){
+        if(validasiInput()){
+            val sip: Response<Void> = repositoryDataSiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+            if (sip.isSuccessful){
+                println("Succes Tambah Data:${sip.message()}")
+            }else{
+                println("gagal Tambah Data: ${sip.errorBody()}")
+            }
+        }
+    }
+}
