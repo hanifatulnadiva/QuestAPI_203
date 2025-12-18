91d0fa14927336dc403540d56f02c41cdaa9d476
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-12-18 19:32:27 +0700

    add route destinasi untuk home

---
+++ app/src/main/java/com/example/apisiswa/uicontroller/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.apisiswa.uicontroller
+
+import com.example.apisiswa.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route ="home"
+    override  val titleRes = R.string.app_name
+}
