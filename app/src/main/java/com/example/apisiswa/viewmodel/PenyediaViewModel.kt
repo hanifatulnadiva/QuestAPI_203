bdc946562c008e3cc57516fdffbe3a63f5e54ee8
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-12-18 19:31:45 +0700

    add penyedia view model untuk menggabungkan semua view model yang ada, jadi nanti yang bakalan dipanggil di ui itu penyedia view model ini

---
+++ app/src/main/java/com/example/apisiswa/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.apisiswa.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.apisiswa.repositori.AplikasiDataSiswa
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa=(
+    this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as AplikasiDataSiswa
+)
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(
+            aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+        initializer {
+            EntryViewModel(
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+
+    }
+}
