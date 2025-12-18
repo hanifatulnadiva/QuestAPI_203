5c9743fc7957d3e9ece9145a254d84da5b1650f7
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-12-18 19:32:12 +0700

    add route destinasi untuk tambah siswa

---
+++ app/src/main/java/com/example/apisiswa/uicontroller/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.apisiswa.uicontroller
+
+import com.example.apisiswa.R
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route ="item_entry"
+    override val titleRes= R.string.entry_siswa
+}
