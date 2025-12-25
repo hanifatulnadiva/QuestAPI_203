67e86e07fb7860dbe5a808c7c41358d43cc7b8b8
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-12-25 15:00:13 +0700

    menambahkan repository untuk detail, update dan delete berdasarkan api service yang sudah dibuat

--- app/src/main/java/com/example/apisiswa/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/apisiswa/repositori/RepositoryDataSiswa.kt
@@ -7,6 +7,10 @@ import retrofit2.Response
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa():List<DataSiswa>
     suspend fun  postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
@@ -14,5 +18,7 @@ class JaringanRepositoryDataSiswa(
 ): RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceAppSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceAppSiswa.postSiswa(dataSiswa)
-
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceAppSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): Response<Void> = serviceAppSiswa.editSatuSiswa(id,dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void> = serviceAppSiswa.hapusSatuSiswa(id)
 }
