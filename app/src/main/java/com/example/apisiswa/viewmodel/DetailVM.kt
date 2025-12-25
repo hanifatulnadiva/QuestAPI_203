5dd65c12d93030a257deee671df6926bb8aba344
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-12-25 17:34:03 +0700

    menambahkan class DetailVM berfungsi sebagai penghubung antara data (Repository) dan tampilan (UI) pada halaman detail siswa, lalu mengambil idsiswa dari route navigasi

--- app/src/main/java/com/example/apisiswa/viewmodel/DetailVM.kt
+++ app/src/main/java/com/example/apisiswa/viewmodel/DetailVM.kt
@@ -1,10 +1,19 @@
 package com.example.apisiswa.viewmodel
 
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.apisiswa.modeldata.DataSiswa
+import com.example.apisiswa.repositori.RepositoryDataSiswa
+import com.example.apisiswa.uicontroller.DestinasiDetail
 
 
 sealed interface StatusUIDetail{
     data class Success(val satusiswa: DataSiswa): StatusUIDetail
     object Error: StatusUIDetail
     object Loading : StatusUIDetail
+}
+class DetailVM (savedStateHandle: SavedStateHandle, private  val repositoryDataSiswa: RepositoryDataSiswa): ViewModel(){
+    private  val idSiswa:Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
 }
