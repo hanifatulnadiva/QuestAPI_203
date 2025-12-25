247ce31c10aba3a9d1fd42f304b800e5321332c6
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-12-25 17:52:21 +0700

    add EditVM mengelola seluruh logika halaman edit siswa: ambil data lama, simpan perubahan form, validasi input, dan update data ke server — UI hanya menampilkan dan merespons state.

---
+++ app/src/main/java/com/example/apisiswa/viewmodel/EditVM.kt
@@ -0,0 +1,53 @@
+package com.example.apisiswa.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.apisiswa.modeldata.DetailSiswa
+import com.example.apisiswa.modeldata.UIStateSiswa
+import com.example.apisiswa.modeldata.toDataSiswa
+import com.example.apisiswa.modeldata.toUIStateSiswa
+import com.example.apisiswa.repositori.RepositoryDataSiswa
+import com.example.apisiswa.uicontroller.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class EditVM(savedStateHandle: SavedStateHandle, private  val  repositoryDataSiswa: RepositoryDataSiswa):
+    ViewModel() {
+
+        var uiStateSiswa by mutableStateOf(UIStateSiswa())
+            private set
+
+    private  val idSiswa:Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa=repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUIStateSiswa(true)
+        }
+    }
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa= UIStateSiswa(detailSiswa=detailSiswa, isEntryValid =validasiInput(detailSiswa))
+    }
+    private  fun validasiInput(uiState: DetailSiswa=uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
+            nama.isNotBlank()&&alamat.isNotBlank()&& telpon.isNotBlank()
+        }
+    }
+    suspend fun editSatuSiswa(){
+        if(validasiInput(uiStateSiswa.detailSiswa)){
+            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa,uiStateSiswa
+                .detailSiswa.toDataSiswa())
+
+            if(call.isSuccessful){
+                println("Update sukses: ${call.message()}")
+            }
+            else{
+                println("Update Error: ${call.errorBody()}")
+            }
+        }
+    }
+
+}
